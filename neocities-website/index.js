2a6c4f19da7884b562db48231f43c376f862bbc1
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2024-02-12 00:52:04 +0100

    feat: Navigating footer hell

--- neocities-website/index.js
+++ neocities-website/index.js
@@ -9,15 +9,19 @@ var this_variable_is_soooooooooo_important_slash_j = undefined;
 class ContentAdders {
   static footer = document.getElementById("footer-paster");
   static #footerPasteContent = `
-        <footer class="footer-display">
-          <img 
-           src="assets/images/neocities.png"
-           onerror="this.src='../assets/images/neocities.png';"
-           alt="Neocities logo with a caption &quot;Powered by Neocities&quot;"> 02.2024 - THIS_VERY_FEMTOSECOND, by ME
-        </footer>
+        <div class="cooler-footer">
+          <footer class="footer-display">
+            <img 
+              src="assets/images/neocities.png"
+              onerror="this.src='../assets/images/neocities.png';"
+              alt="Neocities logo with a caption &quot;Powered by Neocities&quot;">
+            <p>02.2024 - THIS_VERY_FEMTOSECOND, by ME</p>
+          </footer>
+        </div>
         <div class="post-footer">
-          <p>EOF</p>
-        </div> <!-- post-footer -->`;
+          <p>#.EOF_</p>
+        </div>
+`;
 
   static footerPaster() {
     this.footer.innerHTML = this.#footerPasteContent;
