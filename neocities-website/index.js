589b1a2ec4b3cb134f859ffbaa4d945cf85b76cc
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2024-02-11 18:34:55 +0100

    add: Basic project structure

---
+++ neocities-website/index.js
@@ -0,0 +1,7 @@
+/*
+ * NeoReactCities.js, world's #best JavaScript (script for being interpreted by JVM) "framework"!
+ *
+ * Copyleft StarRflowerR -i2024
+*/
+
+var this_variable_is_soooooooooo_important_slash_j = undefined;
